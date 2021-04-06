6af1a9b2ce72147e9945b5bbd38e22c4bc51d7d7
lee sherwood <leesherwood28@googlemail.com>
2021-04-06 15:07:27 +0100

    Begining to setup tailwind correctly

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const plugins = [ tailwindcss, autoprefixer ];
+module.exports = {
+    plugins: plugins
+}
