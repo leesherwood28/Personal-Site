232f15076fcebbc89cbc1950f5f835027cdedac9
lee sherwood <leesherwood28@googlemail.com>
2021-04-10 15:36:22 +0100

    Adding home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       'montserrat': ['Montserrat', 'sans-serif'],
+      'anton': ['Anton', 'sans-serif']
     },
     extend: {
       transitionProperty: {
